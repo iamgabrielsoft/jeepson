7e019caab70edf44bb103d2820ece6493d8cb84b
iamgabrielsoft <iamgabrielsoft@gmail.com>
2022-04-03 19:26:44 +0100

    decompress file

--- src/decompress.ts
+++ src/decompress.ts
@@ -1,5 +1,6 @@
 
 import { OrderedIndex, Cursor, Target, TargetType} from "./decompressor/common";
+import { decompressStages } from "./decompressor/stages";
 
 
 /**
@@ -18,10 +19,54 @@ import { OrderedIndex, Cursor, Target, TargetType} from "./decompressor/common";
   /**
  * Create a new cursor with a root target for specified drain mode
  */
-function makeCursor(drain: boolean): Cursor {
+export const makeCursor = (drain: boolean): Cursor => {
     const rootTarget: Target = { type: TargetType.SCALAR, value: void 0 };
     const stack: Target[] = new Array(10);
     stack[0] = rootTarget;
   
     return { index: 0, rootTarget, stack, currentTarget: rootTarget, pointer: 0, drain };
+}
+
+
+/**
+ * Decompress data string with provided ordered index
+ */
+export const decompress = (data: string, orderedIndex: OrderedIndex) => {
+  const cursor = makeCursor(true); 
+  decompressStages(cursor, data, orderedIndex); 
+  return cursor.rootTarget.value; 
+
+}
+
+/**
+ * Decompress zipson data incrementally by providing each chunk of data in sequence
+ */
+
+export const decompressIncremental = (orderedIndex) => {
+  const cursor = makeCursor(false); 
+
+  let buffer = ''; 
+
+  const increment = (data: string | null) => {
+    if(data == null) {
+      cursor.drain = true; // Move cursor to drain mode if we got the last chunk of data
+    }
+
+    else if(data.length == 0) return 
+    else { 
+      buffer += data; 
+    }
+
+    const cursorIndexBefor = cursor.index; 
+    decompressStages(cursor, buffer, orderedIndex); 
+    const moveAmount = cursor.index - cursorIndexBefor; 
+
+
+    if(moveAmount > 0) {
+      buffer = buffer.substring(moveAmount); 
+      cursor.index -= moveAmount; 
+    }
   }
+
+  return { increment, cursor }
+}
